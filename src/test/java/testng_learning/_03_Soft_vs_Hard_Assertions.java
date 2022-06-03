aaf6d457ea24ab0cec0e95336f372030616a8cd0
dtaskin <taskindeniz26@gmail.com>
2022-06-03 10:36:29 -0400

    TestNG and JUnit and soft assertions added

---
+++ src/test/java/testng_learning/_03_Soft_vs_Hard_Assertions.java
@@ -0,0 +1,39 @@
+package testng_learning;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
+
+public class _03_Soft_vs_Hard_Assertions {
+    /*
+    @Test(priority = 1,description = "Practicing hard assertions")
+    public void method1(){
+
+        //Hard assertions
+        //If there is an error at any line, the program flow will break, and it will not continue with the rest
+        Assert.assertTrue(true);
+        Assert.assertTrue(false, "=======Failure=======");
+
+        System.out.println("Hello world");
+
+    }
+
+    @Test(priority = 2, description = "Practicing soft assertions")
+
+    public void method2(){
+
+        //Soft assertions
+        //
+
+        SoftAssert softAssert = new SoftAssert();
+
+        softAssert.assertTrue(true);
+        softAssert.assertTrue(false,"=======Failure======="); // Assertion error
+
+        System.out.println("Hello world");
+        softAssert.assertAll();
+
+    }
+
+     */
+}
